755810354adf3f31e10e9fceca952884fa9fe1e6
Johnpope7 <Johnpope5387@gmail.com>
2022-08-20 21:56:12 -0700

    Completed enemy and pickup spawners
    Completed enemy and pickup spawners, they now can spawn enemies and pickups when i request

---
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    public List<Transform> waypoints; //list of waypoints for enemy patrols
+    #endregion
+}
--- Assets/Scripts/Navigation/WaypointLister.cs
+++ Assets/Scripts/Navigation/WaypointLister.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class WaypointLister : MonoBehaviour
 {
-    private Transform tf;//stores the transform of the waypoint
+    [SerializeField]
+    public RoomManager roomManager;
+
+    public Transform tf;//stores the transform of the waypoint
     // Start is called before the first frame update
     void Start()
     {
-        tf = GetComponent<Transform>(); //sets the transform of the waypoint
-        GameManager.instance.waypoints.Add(tf);//adds the transform of the waypoint to the list of waypoints
+        if (!tf)
+        {
+            tf = GetComponent<Transform>(); //sets the transform of the waypoint
+        }
+        if (roomManager)
+        {
+            roomManager.waypoints.Add(tf);//adds the transform of the waypoint to the list of waypoints
+        }
     }
 
     // Update is called once per frame
